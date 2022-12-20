341d75c8e4ce3f544c7c1b8455c227b99e2af185
SametBayramOfficial@gmail.com <SametBayramOfficial@gmail.com>
2022-12-20 12:25:18 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DtAhmetYasinBayram.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
